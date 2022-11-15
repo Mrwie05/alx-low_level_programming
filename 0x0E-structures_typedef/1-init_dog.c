14a6e2d49f9ac3be47b9844c8b66afdc3de16ced
Siphiwe Ntombela <111127123+Mrwie05@users.noreply.github.com>
2022-11-15 02:40:44 +0200

    hello

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,17 @@
-#define BUFSIZE 1020
+#include "dog.h"
 
-#define TABLESIZE BUFSIZE
-
-#undef BUFSIZE
-
-#define BUFSIZE 1020
+/**
+ * init_dog - initializes a variable of type struct dog.
+ * struct dog - new structure.
+ * @d: pointer.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ * Return: nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
